08e24e44b0ef3e516454d1186dff5438adfb14a1
tondikattipoorvi <109957550+tondikattipoorvi@users.noreply.github.com>
2022-08-28 19:05:31 +0700

    Add files via upload

---
+++ expt1.py
@@ -0,0 +1,19 @@
+x=int(input("enter the number 1"))
+y=int(input("enter the number 2"))
+sum=x+y
+print("the addition is",sum)
+
+x=int(input("enter the number 3"))
+y=int(input("enter the number 4"))
+sub=x-y
+print("the substraction is",sub)
+
+x=int(input("enter the number 5"))
+y=int(input("enter the number 6"))
+mul=x*y
+print("the multiplication is",mul)
+
+x=int(input("enter the number 7"))
+y=int(input("enter the number 8"))
+div=x/y
+print("the division is",div)
